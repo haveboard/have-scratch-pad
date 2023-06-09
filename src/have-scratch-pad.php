b15f5eaa74a7842ead58888d9afd5b48488386a1
FinnATM <jonathan.finnegan@allthingsmedia.com>
2023-06-09 16:46:24 -0400

    add main files to src dir

---
+++ src/have-scratch-pad.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The initation loader for have-scratch-pad main plugin file.
+ *
+ * @category     WordPress_Plugin
+ * @license      MIT
+ * Plugin Name:  have-scratch-pad
+ * Description:  
+ * Author:       Jonathan Finnegan
+ * Author URI:   https://jonathanfinnegan.com
+ * Contributors: Jonathan Finnegan
+ *
+ * Version:      0.1 Beta
+ *
+ * Text Domain:  hvscrtchpd
+ * Domain Path:  languages
+ *
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * https://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * **********************************************************************
+ */
+
+require_once 'have-scratch-pad/have-scratch-pad.php';
